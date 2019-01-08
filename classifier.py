fecec70c7e0de149f0e5e5511c5fd23f025f2bb0
shovalul <shovalul@campus.technion.ac.il>
2019-01-08 21:39:18 +0200

    ex 3.1

--- classifier.py
+++ classifier.py
@@ -1,7 +1,23 @@
 import hw3_utils
+import pickle
+import math
+
 
 def euclidean_distance(feature_set1, feature_set2):
     return sum(list(map(lambda tup: (tup[0] - tup[1]) ** 2, zip(feature_set1, feature_set2)))) ** 0.5
+
+def split_crosscheck_groups(dataset, num_folds):
+    features_indexes_true = [i for i in range(len(dataset[0])) if dataset[1][i] == True]
+    features_indexes_false = [i for i in range(len(dataset[0])) if dataset[1][i] == False]
+    factor_true = math.ceil(len(features_indexes_true) / float(num_folds))
+    factor_false = math.ceil(len(features_indexes_false) / float(num_folds))
+
+    for i in range(num_folds):
+        dataset_true_i = features_indexes_true[i*factor_true : (i+1)*factor_true]
+        dataset_false_i = features_indexes_false[i*factor_false : (i+1)*factor_false]
+        dataset_to_write = ([dataset[0][i] for i in dataset_true_i] + [dataset[0][i] for i in dataset_false_i],
+                            [dataset[1][i] for i in dataset_true_i] + [dataset[1][i] for i in dataset_false_i])
+        pickle.dump(dataset_to_write, open("ecg_fold_" + str(i+1) + ".data", 'wb'))
 
 
 
@@ -18,8 +34,6 @@ class knn_classifier(hw3_utils.abstract_classifier):
         dataset = list(map(lambda x: x[1], sorted(distances, key=lambda x: x[0])[:self.k]))
         trues = sum(list(map(lambda x: 1 if self.labels[x] == True else 0, dataset)))
         return trues >= self.k / 2
-
-
 
 
 class knn_factory(hw3_utils.abstract_classifier_factory):
--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 import hw3_utils, classifier
 import os
+import pickle
 
+
+def load_data_try(path=r'data/data.pickle'):
+    '''
+    return the dataset that will be used in HW 3
+    prameters:
+    :param path: the path of the csv data file (default value is data/ecg_examples.data)
+
+    :returns: a tuple train_features, train_labels ,test_features
+    features - a numpy matrix where  the ith raw is the feature vector of patient i.
+    '''
+    with open(path,'rb') as f:
+        train_features, train_labels = pickle.load(f)
+    return train_features, train_labels
 
 
 def main():
     train_features, train_labels, test_features = hw3_utils.load_data()
     knn = classifier.knn_factory(3).train(train_features, train_labels)
     print(knn.classify(test_features[59]))
+
+    classifier.split_crosscheck_groups((train_features, train_labels), 2)
+    train_features, train_labels = load_data_try('ecg_fold_1.data')
+    train_features2, train_labels2 = load_data_try('ecg_fold_2.data')
+    print(sum([1 for label in train_labels if label]))
+    print(sum([1 for label in train_labels2 if label]))
+    print(sum([1 for label in train_labels if not label]))
+    print(sum([1 for label in train_labels2 if not label]))
 
 
     # data = [[1,1,1],[2,2,2],[100,3,3]]
